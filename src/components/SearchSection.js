e0d2cbd8e3578325ef6877475505c2e451b6b633
bakroaster <bakroaster@gmail.com>
2021-11-04 17:04:23 +0530

    gg

--- src/components/SearchSection.js
+++ src/components/SearchSection.js
@@ -1,7 +1,5 @@
 import Card from "./Card";
-import InfiniteScroll from "react-infinite-scroll-component";
 import PulseLoader from "react-spinners/PulseLoader";
-import {useLocation} from 'react-router-dom'
 import {
   collection,
   getDocs,
@@ -9,94 +7,69 @@ import {
   limit,
   query,
   startAfter,
-  orderBy,where,
-  startAt,
-  endAt
+  orderBy,
+  where,
 } from "firebase/firestore";
-import fic from "../static/fic.png";
+import { useLocation } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { useEffect, useState } from "react";
 const db = getFirestore();
 
-const SearchSection = () => {
-  // let query = new URLSearchParams(useLocation().search);
-  // console.log(query.get("q"));
+const Section = (props) => {
+  let params = new URLSearchParams(window.location.search);
+
+  let q = params.get("q");
+  console.log(q);
+
   const [posts, setPosts] = useState([]);
-  const [lastKey, setLastKey] = useState("");
-  const [nextPostsloading, setNextPostsLoading] = useState(false);
-  const [hasMore, sethasMore] = useState(true);
-  const [size, setSize] = useState(0);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    // first 5 posts
-    postsFirstBatch()
-      .then((res) => {
-        setPosts(res.posts);
-        setLastKey(res.lastKey);
-        setSize(res.siz);
-        setLoading(false);
-      })
-      .catch((e) => {
-        toast.error(e);
-        console.log(e)
-      });
+    postsFirstBatch();
   }, []);
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
 
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
   const postsFirstBatch = async () => {
     try {
-      const getSize = await getDocs(collection(db, "videos"));
-      let siz = getSize.size;
-      const data = await getDocs(
-        query(collection(db, "videos"),where("videoName", ">=", "a")
-        ,where("videoName", "<=", "a" + "\uf8ff"))
-      );
-      let posts = [];
-      let lastKey = "";
-      data.forEach((doc) => {
-        posts.push(doc.data());
-        lastKey = doc;
-      });
-      if (siz === posts.length) {
-        sethasMore(false);
+      const videoRef = collection(db, "videos");
+
+      if (props.orderBy === "random") {
+        const data = await getDocs(query(videoRef, limit(6)));
+        let posts = [];
+        data.forEach((doc) => {
+          posts.push(doc.data());
+        });
+        setPosts(shuffle(posts));
+        setLoading(false);
       } else {
-        sethasMore(true);
+        const data = await getDocs(
+          query(videoRef, orderBy(props.orderBy, "desc"), limit(6))
+        );
+        let posts = [];
+        data.forEach((doc) => {
+          posts.push(doc.data());
+        });
+        setPosts(posts);
+        setLoading(false);
       }
-      return { posts, lastKey, siz };
     } catch (e) {
       toast.error(e);
-      console.log(e)
-    }
-  };
-
-  const fetchMorePosts = async (key) => {
-    setNextPostsLoading(true);
-    try {
-      const data = await getDocs(
-        query(
-          collection(db, "videos"),
-          limit(4),
-          orderBy("views"),
-          startAfter(key)
-        )
-      );
-      let post = [];
-      data.forEach((doc) => {
-        post.push(doc.data());
-        setLastKey(doc);
-      });
-      setSize(size + post.length);
-
-      if (posts.length !== size) {
-        sethasMore(true);
-      } else {
-        sethasMore(false);
-      }
-      setTimeout(() => {
-        setPosts(posts.concat(post));
-      }, 1500);
-    } catch (e) {
-      toast.catch(e);
-      console.log(e)
     }
   };
 
@@ -138,39 +111,20 @@ const SearchSection = () => {
       {loading ? (
         <div
           id="loader"
-          className="bg-gray-100 w-full h-screen flex justify-center items-center m-0"
+          className="bg-gray-900 w-full h-screen flex justify-center items-center m-0"
         >
           <PulseLoader color={"#b5b5b5"} loading={true} size={20} />
         </div>
-      ) : posts.length !== 0 ? (
-        <InfiniteScroll
-          className="bg-gray-100"
-          dataLength={posts.length}
-          next={() => {
-            fetchMorePosts(lastKey);
-          }}
-          hasMore={hasMore}
-          loader={
-            <div className="p-8" style={{ textAlign: "center" }}>
-              <PulseLoader color={"#b5b5b5"} loading={hasMore} size={20} />
-            </div>
-          }
-          endMessage={<></>}
-        >
-          <div className="grid grid-cols-1 2xl:grid-cols-4 sm:grid-cols-2 xl:grid-cols-3 sm:p-8 bg-gray-100">
-            {allPosts}
-          </div>
-        </InfiniteScroll>
       ) : (
         <div
-          className=" w-full flex justify-center	 items-center	 h-screen"
-          style={{ backgroundColor: "white" }}
+          className="grid grid-cols-1 2xl:grid-cols-3 sm:grid-cols-2 xl:grid-cols-3 "
+          style={{ backgroundColor: "#191c23" }}
         >
-          <img src={fic} alt="error" width="300px" />
+          {allPosts}
         </div>
       )}
     </>
   );
 };
 
-export default SearchSection;
+export default Section;
